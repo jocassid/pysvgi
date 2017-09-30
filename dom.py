5371fec65461ba3efb2636341a73d588134056ab
John Cassidy <jocassid@gmail.com>
2017-09-29 21:56:11 -0400

    added additional dom functionality

--- dom.py
+++ dom.py
@@ -2,42 +2,62 @@
 
 
 class Node(list):
-    
+
+    ELEMENT_NODE = 1
+    ATTRIBUTE_NODE = 2
+    TEXT_NODE = 3
+    CDATA_SECTION_NODE = 4
+    ENTITY_REFERENCE_NODE = 5
+    ENTITY_NODE = 6
+    PROCESSING_INSTRUCTION_NODE = 7
+    COMMENT_NODE = 8
+    DOCUMENT_NODE = 9
+    DOCUMENT_TYPE_NODE = 10
+    DOCUMENT_FRAGMENT_NODE = 11
+    NOTATION_NODE = 12
+
+    def __init__(self, nodeType):
+        super().__init__()
+        self.nodeType = nodeType
+
     def toxml(self, encoding):
         return self.toprettyxml(indent='', newl='', encoding=encoding)
-    
-    def toprettyxml(self, indent='\t', newl='\n', encoding=''):
+
+    def toprettyxml(self, indent='\t', newl='\n', encoding='', indentLevel=0):
         return ''
-        
-     
+
+
 class TextNode(Node):
     def __init__(self, text):
+        super().__init__(Node.TEXT_NODE)
         self.text = text
-        
+
     def __str__(self):
         return self.text
-        
-    def toprettyxml(self, indent='\t', newl='\n', encoding=''):
+
+    def toprettyxml(self, indent='\t', newl='\n', encoding='', indentLevel=0):
         return self.text
 
+
 class Attr(Node):
-    pass
+    def __init__(self):
+        super().__init__(Node.ATTRIBUTE_NODE)
+
 
 class Element(Node):
     DEFAULTS = ()
-    
+
     def __init__(
-            self, 
-            tagName, 
-            namespaceURI=None, 
-            prefix=None, 
+            self,
+            tagName,
+            namespaceURI=None,
+            prefix=None,
             localName=None,
-            **kwargs):  
-        super().__init__()
+            **kwargs):
+        super().__init__(Node.ELEMENT_NODE)
         self.tagName = tagName
         self._attributes = kwargs
 
-        
     @property
     def attributes(self):
         return self._attributes
@@ -49,13 +69,13 @@ class Element(Node):
             if key in attr_dict:
                 continue
             self._attributes[key] = default
-            
+
     def setAttribute(self, name, value):
         self._attributes[name] = value
-        
+
     def getAttribute(self, name):
         return self._attributes[name]
-        
+
     def __setitem__(self, key, value):
         key_type = type(key)
         if key_type == int:
@@ -65,8 +85,8 @@ class Element(Node):
             self._attributes[key] = value
             return
         raise TypeError("Unsupported index type %s" % key_type)
-        
-        
+
+
     def __getitem__(self, key):
         key_type = type(key)
         if key_type == int:
@@ -74,33 +94,60 @@ class Element(Node):
         if key_type == str:
             return self._attributes[key]
         raise TypeError("Unsupported index type %s" % key_type)
-        
-               
-    def toprettyxml(
-        self, 
-        indent='\t', newl='\n', encoding='', indentLevel=0):
-        
+
+    def openingTag(self):
         pieces = ['<', self.tagName]
         for key, value in self.attributes.items():
             pieces.append(' %s="%s"' % (key,value))
         if len(self) == 0:
             pieces.append('/>')
-            return ''.join(pieces)
+            return ''.join(pieces), False
         pieces.append(">")
-        pieces.append(newl)
+        return ''.join(pieces), True
+
+
+    def hasElementChild(self):
         for child in self:
-            pieces.append(indent)
-            pieces.append(child.toprettyxml())
+            if child.nodeType == Node.ELEMENT_NODE:
+                return True
+        return False
+
+
+    def toprettyxml(
+        self,
+        indent='\t',
+        newl='\n',
+        encoding='',
+        indentLevel=0):
+
+        pieces = [indent * indentLevel]
+
+        openingTag, needsClosingTag = self.openingTag()
+        if not needsClosingTag:
+            return indent * indentLevel + openingTag
+
+        pieces.append(openingTag)
+        hasElementChild = self.hasElementChild()
+        if hasElementChild:
             pieces.append(newl)
+        for child in self:
+            pieces.append(
+                child.toprettyxml(
+                    indent,
+                    newl,
+                    encoding,
+                    indentLevel+1))
+            if hasElementChild:
+                pieces.append(newl)
         pieces.append("</%s>" % self.tagName)
         return ''.join(pieces)
-        
+
     def append(self, item):
         if isinstance(item, str):
             item = TextNode(item)
         super().append(item)
-            
-        
+
+
 class Document:
     # I forget what the technical term is for this
     XML_LINE = "<?xml version='1.0' encoding='utf-8'?>"
--- test_dom.py
+++ test_dom.py
@@ -4,56 +4,109 @@
 
 from unittest import TestCase
 
-from dom import Element, TextNode
+from dom import Element, Node, TextNode
 
 
 class NodeTest(TestCase):
-    
+
     def test_append(self):
         div1 = Element('div')
         div2 = Element('div')
         div1.append(div2)
         self.assertEqual(1, len(div1))
-        
+
         childElement = div1[0]
         self.assertTrue(div2 is childElement)
-        
+
         p = Element('p')
         p.append('some text')
         self.assertEqual(1, len(p))
-        
+
         textNode = p[0]
         self.assertTrue(isinstance(textNode, TextNode))
-        
-        
-        
+
+
+class TextNodeTest(TestCase):
+    def test_init(self):
+        node = TextNode('foo')
+        self.assertTrue(hasattr(node, 'nodeType'))
+        self.assertEqual(Node.TEXT_NODE, node.nodeType)
+
+
 class ElementTest(TestCase):
-    
+
     def test_setAttribute(self):
         foo = Element('foo')
-        
+
         # Using standard DOM methods
         foo.setAttribute('alpha', '1')
         self.assertEqual('1', foo.getAttribute('alpha'))
-        
+
         # Using modified __setitem__ and __getitem__
         foo['bravo'] = '2'
         self.assertEqual('2', foo['bravo'])
-        
-        
-    def test_toprettyxml(self):     
+
+    def test_setDefaults(self):
+        class YadaYada(Element):
+            DEFAULTS = (('foo', 'bar'))
+
+            def __init__(self):
+                super().__init__('yadayada')
+        yadaYada = YadaYada()
+        self.assertEqual()
+
+
+    def test_toprettyxml(self):
         svg = Element('svg')
         self.assertEqual('<svg/>', svg.toprettyxml())
-        
+
         svg.setAttribute('width', '200')
         self.assertEqual('<svg width="200"/>', svg.toprettyxml())
-        
-        
+
+        p = Element('p')
+        p.append('some text')
+        self.assertEqual('<p>some text</p>', p.toprettyxml())
+
+        div = Element('div')
+
+        p = Element('p')
+        p.append('one')
+        div.append(p)
+        self.assertEqual(
+            '<div>\n\t<p>one</p>\n</div>',
+            div.toprettyxml())
+
+        p = Element('p')
+        p.append('two')
+        div.append(p)
+
+        self.assertEqual(
+            '<div>\n\t<p>one</p>\n\t<p>two</p>\n</div>',
+            div.toprettyxml())
+
     def test_getitem(self):
-        self.fail("Not implemented")
-        
-        
-    
-        
-        
-        
+        className = 'description'
+        attributes = {'class': className}
+        div = Element('div', **attributes)
+
+        p = Element('p')
+        div.append(p)
+
+        href = 'http://www.python.org'
+        a = Element('a', href=href)
+        div.append(a)
+
+        self.assertIs(p, div[0])
+        self.assertEqual(div['class'], 'description')
+        self.assertIs(a, div[1])
+        self.assertEqual(href, div[1]['href'])
+
+    def test_hasElementChildren(self):
+        div = Element('div')
+        div.append('foo')
+        self.assertEqual(False, div.hasElementChild())
+
+        div = Element('div')
+        div.append('text child')
+        div.append(Element('hr'))
+        self.assertEqual(True, div.hasElementChild())
