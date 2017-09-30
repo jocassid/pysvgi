2006872e66d9426640c6fd06a4b4134356aac9f1
John Cassidy <jocassid@gmail.com>
2017-09-29 22:00:00 -0400

    work in progress

--- test_CyclicTimeline.py
+++ test_CyclicTimeline.py
@@ -5,19 +5,17 @@ from unittest import TestCase
 
 from CyclicTimeline import DatetimeUtil
 
+
 class DatetimeUtilTest(TestCase):
-   
+
     def test_round_down(self):
-        d1 = datetime(2017, 9, 17, 2, 13, 9, 31434) 
+        d1 = datetime(2017, 9, 17, 2, 13, 9, 31434)
         self.assertEqual(
             datetime(2017, 9, 17, 2),
             DatetimeUtil.round_down(d1, 'hour'))
-        
+
     def test_round_up(self):
-        d1 = datetime(2017, 9, 17, 2, 13, 9, 31434) 
+        d1 = datetime(2017, 9, 17, 2, 13, 9, 31434)
         self.assertEqual(
             datetime(2017, 9, 17, 3),
             DatetimeUtil.round_up(d1, 'hour'))
-        
-    
-        
--- test_pysvgi.py
+++ test_pysvgi.py
@@ -2,23 +2,22 @@
 from unittest import TestCase
 
 from pysvgi import Svg
-        
-        
+
+
 class SvgTest(TestCase):
-    
+
     def test_init(self):
         svg = Svg()
         self.assertEqual("1.1", svg['version'])
         self.assertEqual("full", svg['baseProfile'])
         self.assertEqual("http://www.w3.org/2000/svg", svg['xmlns'])
-            
-    
+
     def test_width(self):
         svg = Svg()
         svg.width = 200
         self.assertEqual(200, svg.attributes['width'])
         self.assertEqual(200, svg.width)
-        
+
     def test_height(self):
         svg = Svg()
         svg.height = 300
